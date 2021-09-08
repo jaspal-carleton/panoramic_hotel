abb5092d3d374603709d8935a4cda92b3f2793d2
Jaspal Singh <jaspal.singh@carleton.ca>
2021-09-08 08:38:07 -0400

    added tests to check same booking dates

--- test/routes/routes.test.js
+++ test/routes/routes.test.js
@@ -39,6 +39,26 @@ describe("Test Suite - Router", function () {
                     throw err;
                 });
         });
+
+        it("should fail to create booking for same dates", function (done) {
+            chai.request('http://localhost:5000')
+                .post('/api/v1/booking')
+                .send({
+                    "email": "jane.doe@example.com",
+                    "first_name": "Jane",
+                    "last_name": "Doe",
+                    "guest_count": 2,
+                    "checkin_date": "2022-12-10",
+                    "checkout_date": "2022-12-12"
+                })
+                .then(function (res) {
+                    expect(res).to.have.status(409);
+                    done();
+                })
+                .catch(function (err) {
+                    throw err;
+                });
+        });
     });
 
     describe("GET /api/v1/booking", function () {
@@ -53,14 +73,38 @@ describe("Test Suite - Router", function () {
                     throw err;
                 });
         });
+
+        it("should fail to get wrong booking", function (done) {
+            chai.request('http://localhost:5000')
+                .get('/api/v1/booking/xyz')
+                .then(function (res) {
+                    expect(res).to.have.status(404);
+                    done();
+                })
+                .catch(function (err) {
+                    throw err;
+                });
+        });
     });
 
     describe("DELETE /api/v1/booking", function () {
-        it("should get existing booking", function (done) {
+        it("should delete existing booking", function (done) {
             chai.request('http://localhost:5000')
                 .delete('/api/v1/booking/' + bookingId)
                 .then(function (res) {
                     expect(res).to.have.status(200);
+                    done();
+                })
+                .catch(function (err) {
+                    throw err;
+                });
+        });
+
+        it("should fail to delete wrong booking", function (done) {
+            chai.request('http://localhost:5000')
+                .delete('/api/v1/booking/xyz')
+                .then(function (res) {
+                    expect(res).to.have.status(404);
                     done();
                 })
                 .catch(function (err) {
--- test/services/services.test.js
+++ test/services/services.test.js
@@ -30,6 +30,20 @@ describe("Test Suite - Booking Service Functions", function () {
                 bookingId = res.id;
             })
         });
+
+        it("should not create booking with same dates", function () {
+            const result = serviceBooking.createBooking(
+                "jane.doe@example.com",
+                "Jane",
+                "Doe",
+                2,
+                "2022-12-10",
+                "2022-12-12"
+            );
+            result.then( res => {
+                expect(res.status).to.be.false;
+            })
+        });
     });
 
     describe("Fetch Booking", function () {
@@ -39,6 +53,13 @@ describe("Test Suite - Booking Service Functions", function () {
                 expect(res.data).to.have.property('email');
             })
         });
+
+        it("should fail to retrieve wrong booking", function () {
+            const result = serviceBooking.fetchBooking("xyz");
+            result.then( res => {
+                expect(res.status).to.be.false;
+            })
+        });
     });
 
     describe("Delete Booking", function () {
@@ -46,6 +67,13 @@ describe("Test Suite - Booking Service Functions", function () {
             const result = serviceBooking.deleteBooking(bookingId);
             result.then( res => {
                 expect(res.status).to.be.true;
+            })
+        });
+
+        it("should fail to delete wrong booking", function () {
+            const result = serviceBooking.deleteBooking("xyz");
+            result.then( res => {
+                expect(res.status).to.be.false;
             })
         });
     });
